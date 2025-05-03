62beb3c8ea4a50bd3cb0f375ebbfd264729341ba
vasylky <vasylqvky@gmail.com>
2025-05-03 12:48:54 +0300

    change main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,3 +74,10 @@ resource "azurerm_subnet" "subnet2" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.3.0/24"]
 }
+
+resource "azurerm_subnet" "subnet3" {
+  name                 = "subnet-example3"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
