676efb2e6f84a8994e8b9aa16531be9389c197d8
vasylky <vasylqvky@gmail.com>
2025-05-03 12:32:08 +0300

    add new subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -67,3 +67,10 @@ resource "azurerm_subnet" "subnet1" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet-example2"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
